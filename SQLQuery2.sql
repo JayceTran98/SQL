25740d618bf87a5b4b1fdf087a4bd46d59084e2c
JayceTran <tranhoangphuc943@gmail.com>
2023-05-18 23:28:58 +0700

    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,202 @@
+﻿--1.In ra danh sách (mã học viên, họ tên, ngày sinh, mã lớp) lớp trưởng của các lớp.
+select b.mahv,b.ho,b.ten,b.ngsinh,b.malop
+from lop a left join hocvien b on a.trglop = b.mahv
+
+--2.In ra bảng điểm khi thi (mã học viên, họ tên , lần thi, điểm số) môn CTRR của lớp “K12”, sắp xếp theo tên, họ học viên.
+select a.mahv,a.ho,a.ten,b.lt,b.diem
+from hocvien a left join ketquathi b on a.mahv = b.mahv
+where a.malop = 'k12' and b.mamh = 'CTRR'
+order by a.ten,a.ho
+
+--3.In ra danh sách những học viên (mã học viên, họ tên) và những môn học mà học viên đó thi lần thứ nhất đã đạt.
+select a.mahv,a.ho,a.ten,b.mamh
+from hocvien a left join ketquathi b on a.mahv = b.mahv
+where lt = 1 and kqua = 'dat'
+
+--4.In ra danh sách học viên (mã học viên, họ tên) của lớp “K11” thi môn CTRR không đạt (ở lần thi 1).
+select a.mahv,a.ho,a.ten
+from hocvien a left join ketquathi b on a.mahv = b.mahv
+where a.malop = 'k11' and b.mamh='CTRR' and lt = 1 and b.kqua ='khong dat'
+
+--5.* Danh sách học viên (mã học viên, họ tên) của lớp “K” thi môn CTRR không đạt (ở tất cả các lần thi).
+select a.mahv,a.ho,a.ten
+from hocvien a left join ketquathi b on a.mahv = b.mahv
+where a.malop like 'k%' and b.mamh ='CTRR' and b.kqua ='khong dat'
+
+--6.Tìm tên những môn học mà giáo viên có tên “Tran Tam Thanh” dạy trong học kỳ 1 năm 2006.
+select c.tenmh
+from giaovien a inner join giangday b on a.magv = b.magv
+inner join monhoc c on b.mamh = c.mamh 
+where a.hoten = 'Tran Tam Thanh' and b.hocky = 1 and nam = 2006
+
+--7.Tìm những môn học (mã môn học, tên môn học) mà giáo viên chủ nhiệm lớp “K11” dạy trong học kỳ 1 năm 2006.
+select a.mamh,a.tenmh
+from monhoc a inner join giangday b on a.mamh = b.mamh
+inner join lop c on b.magv = c.magvcn
+where b.malop = 'k11' and b.hocky = 1 and nam = 2006
+
+--8.Tìm họ tên lớp trưởng của các lớp mà giáo viên có tên “Nguyen To Lan” dạy môn “Co So Du Lieu”.
+select a.ho,a.ten
+from hocvien a inner join lop b on a.mahv = b.trglop
+inner join giangday c on b.malop = c.malop
+inner join giaovien d on c.magv =d.magv
+inner join monhoc e on c.mamh = e.mamh
+where d.hoten = 'Nguyen To Lan' and e.tenmh = 'Co So Du Lieu'
+
+--9.In ra danh sách những môn học (mã môn học, tên môn học) phải học liền trước môn “Co So Du Lieu”.
+select a.mamh,a.tenmh
+from MONHOC a inner join DIEUKIEN b on a.MAMH =b.MAMH_TRUOC
+where b.MAMH ='CSDL'
+
+--10.Môn “Cau Truc Roi Rac” là môn bắt buộc phải học liền trước những môn học (mã môn học, tên môn học) nào.
+select a.mamh,a.tenmh
+from MONHOC a inner join DIEUKIEN b on a.MAMH =b.MAMH
+where b.MAMH_TRUOC ='CTRR'
+
+--11.Tìm họ tên giáo viên dạy môn CTRR cho cả hai lớp “K11” và “K12” trong cùng học kỳ 1 năm 2006.
+select a.hoten
+from giaovien a inner join giangday b on a.magv = b.magv
+where b.malop = 'k11' and b.hocky = 1 and nam = 2006 and b.mamh = 'CTRR'
+intersect
+select a.hoten
+from giaovien a inner join giangday b on a.magv = b.magv
+where b.malop = 'k12' and b.hocky = 1 and nam = 2006 and b.mamh = 'CTRR'
+
+--12.Tìm những học viên (mã học viên, họ tên) thi không đạt môn CSDL ở lần thi thứ 1 nhưng chưa thi lại môn này.
+ with count_lt as (
+ select mahv,count(mahv) as count_lt 
+			from ketquathi 
+			where (mamh ='CSDL' and kqua ='khong dat')  
+			group by mahv 
+			having count(mahv) =1
+			)
+select a.mahv,a.ho,a.ten
+from hocvien a inner join count_lt b on a.mahv = b.mahv
+
+--13.	Tìm giáo viên (mã giáo viên, họ tên) không được phân công giảng dạy bất kỳ môn học nào.
+select a.magv,a.hoten
+from giaovien a
+where a.magv not in (select magv from giangday)
+
+--14.Tìm giáo viên (mã giáo viên, họ tên) không được phân công giảng dạy bất kỳ môn học nào thuộc khoa giáo viên đó phụ trách.
+
+--15.Tìm họ tên các học viên thuộc lớp “K11” thi một môn bất kỳ quá 3 lần vẫn “Khong dat” hoặc thi lần thứ 2 môn CTRR được 5 điểm.
+select a.ho,a.TEN
+from HOCVIEN a inner join KETQUATHI b on a.MAHV = b.MAHV
+where (a.MALOP = 'k11' and b.LT >= 3 and b.KQUA = 'khong dat') or (b.LT = 2 and b.MAMH = 'CTRR' and b.DIEM = 5)
+
+--16.Tìm họ tên giáo viên dạy môn CTRR cho ít nhất hai lớp trong cùng một học kỳ của một năm học.
+with count_class as (
+select MAGV,count(malop) as count_class
+from GIANGDAY
+where MAMH = 'CTRR' and HOCKY = 1
+group by MAGV
+having count(malop) >= 2
+union
+select MAGV,count(malop) as count_class
+from GIANGDAY
+where MAMH = 'CTRR' and HOCKY = 2
+group by MAGV
+having count(malop) >= 2
+union
+select MAGV,count(malop) as count_class
+from GIANGDAY
+where MAMH = 'CTRR' and HOCKY = 3
+group by MAGV
+having count(malop) >= 2
+)
+
+select a.MAGV,a.HOTEN
+from GIAOVIEN a inner join count_class b on a.MAGV = b.MAGV
+
+--17.Danh sách học viên và điểm thi môn CSDL (chỉ lấy điểm của lần thi sau cùng).
+with diem_thi as(
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY mahv ORDER BY ngthi DESC) AS rn
+    FROM KETQUATHI
+	WHERE mamh = 'CSDL')
+
+select a.ho,a.TEN,b.DIEM
+from HOCVIEN a inner join diem_thi b on a.MAHV = b.MAHV
+where rn = 1
+
+--18.Danh sách học viên và điểm thi môn “Co So Du Lieu” (chỉ lấy điểm cao nhất của các lần thi).
+with diem_thi as(
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY mahv ORDER BY diem DESC) AS rn
+    FROM KETQUATHI
+	WHERE mamh = 'CSDL')
+
+select a.ho,a.TEN,b.DIEM
+from HOCVIEN a inner join diem_thi b on a.MAHV = b.MAHV
+where rn = 1
+
+--19.	Khoa nào (mã khoa, tên khoa) được thành lập sớm nhất.
+select MAKHOA,TENKHOA
+from khoa
+where NGTLAP <= (select min(NGTLAP) from KHOA)
+
+--20.	Có bao nhiêu giáo viên có học hàm là “GS” hoặc “PGS”.
+select HOCHAM,count(hocham) as num
+from GIAOVIEN
+group by HOCHAM
+having HOCHAM ='GS' or HOCHAM = 'PGS'
+
+--21.Thống kê có bao nhiêu giáo viên có học vị là “CN”, “KS”, “Ths”, “TS”, “PTS” trong mỗi khoa.
+select hocvi,count(hocvi),makhoa
+from giaovien
+group by makhoa,hocvi
+
+--22.Mỗi môn học thống kê số lượng học viên theo kết quả (đạt và không đạt).
+select mamh,kqua,count(kqua)as [count] 
+from ketquathi
+group by mamh,kqua
+
+--23.Tìm giáo viên (mã giáo viên, họ tên) là giáo viên chủ nhiệm của một lớp, đồng thời dạy cho lớp đó ít nhất một môn học.
+select a.magv,a.hoten
+from giaovien a inner join lop b on a.magv = b.magvcn
+inner join giangday c on b.magvcn = c.magv
+where c.malop = b.malop
+
+--24.Tìm họ tên lớp trưởng của lớp có sỉ số cao nhất.
+select b.ho,b.ten
+from lop a inner join hocvien b on a.trglop = b.mahv
+where a.siso >= (select max(siso) from lop)
+
+
+--25.* Tìm họ tên những LOPTRG thi không đạt quá 3 môn (mỗi môn đều thi không đạt ở tất cả các lần thi).
+select c.ho,c.ten
+from lop a inner join ketquathi b on a.trglop = b.mahv
+inner join hocvien c on c.mahv = b.mahv
+where b.kqua = 'khong dat'
+
+--26.Tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9,10 nhiều nhất.
+select b.mahv,a.ho,a.ten,b.diem,count(diem)
+from hocvien a inner join ketquathi b on a.mahv = b.mahv
+group by b.mahv,a.ho,a.ten,b.diem
+having (b.diem between 9 and 10) and count(diem) >= 2
+
+--27.Trong từng lớp, tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9,10 nhiều nhất.
+select b.mahv,a.ho,a.ten,b.diem,a.MALOP,count(diem)
+from hocvien a inner join ketquathi b on a.mahv = b.mahv
+group by b.mahv,a.ho,a.ten,b.diem,a.MALOP
+having (b.diem between 9 and 10) and count(diem) >= 2
+
+--28.Trong từng học kỳ của từng năm, mỗi giáo viên phân công dạy bao nhiêu môn học, bao nhiêu lớp.
+select MAGV,HOCKY,count(mamh) as Sl_MH, count(malop) as sl_lop
+from GIANGDAY
+group by MAGV,HOCKY
+--29.Trong từng học kỳ của từng năm, tìm giáo viên (mã giáo viên, họ tên) giảng dạy nhiều nhất.
+select a.MAGV,b.HOTEN,a.HOCKY,count(a.mamh) as Sl_MH
+from GIANGDAY a inner join GIAOVIEN b on a.MAGV = b.MAGV
+group by a.MAGV,a.HOCKY,b.HOTEN
+having count(a.mamh) >= 2
+--30.Tìm môn học (mã môn học, tên môn học) có nhiều học viên thi không đạt (ở lần thi thứ 1) nhất.
+with SL as (
+select b.MAMH,a.TENMH,count(b.kqua) as num
+from MONHOC a inner join KETQUATHI b on a.MAMH = b.MAMH
+where b.lt =1 and b.KQUA = 'khong dat'
+group by b.MAMH,a.TENMH
+)
+select mamh,tenmh
+from SL
+where num >= (select max(num) from sl)
+
---
+++ SQLQuery_bai1.sql
@@ -0,0 +1,284 @@
+﻿-- 1.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất. --
+SELECT MASP , TENSP
+FROM SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+
+--2.	In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+select masp,tensp
+from sanpham
+where dvt in('cay','quyen')
+
+--3.	In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
+select masp,tensp
+from sanpham
+where masp like 'B%01' 
+
+--4.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
+select masp,tensp
+from sanpham
+where nuocsx = 'Trung Quoc' and gia between 30000 and 40000
+
+--5.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
+select masp,tensp
+from sanpham
+where nuocsx in('Trung Quoc','Thai Lan') and gia between 30000 and 40000
+
+--6.	In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+select sohd,trigia,nghd
+from hoadon
+where nghd in ('1/1/2007','2/1/2007')
+
+--7.	In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
+select sohd,trigia,nghd
+from hoadon
+where month(nghd) = 1
+order by nghd , trigia desc
+
+--8.	In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+select a.makh,a.hoten
+from khachhang a left join hoadon b on a.makh = b.makh
+where b.nghd = '1/1/2007'
+
+--9.	In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.--
+select a.hoten,b.sohd,b.trigia,b.nghd
+from nhanvien a left join hoadon b on a.manv = b.manv
+where a.hoten = 'Nguyen Van B' and b.nghd = '2006/10/28'
+
+
+--10.	In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.--
+select a.masp,a.tensp , d.hoten, c.nghd
+from sanpham a left join cthd b on a.masp = b.masp
+left join hoadon c on b.sohd = c.sohd
+inner join khachhang d on c.makh = d.makh
+where d.hoten = 'Nguyen Van A' and month(c.nghd) = 10 and year(c.nghd) = 2006
+
+--11.	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+select distinct a.sohd
+from hoadon a left join cthd b on a.sohd = b.sohd
+where b.masp in('BB01','BB02')
+
+--12.	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+select distinct a.sohd
+from hoadon a left join cthd b on a.sohd = b.sohd
+where b.masp in('BB01','BB02') and b.sl between 10 and 20
+
+--13.	Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+select a.sohd
+from hoadon a left join cthd b on a.sohd = b.sohd
+where b.masp ='BB01' and b.sl between 10 and 20
+intersect
+select a.sohd
+from hoadon a left join cthd b on a.sohd = b.sohd
+where b.masp ='BB02' and b.sl between 10 and 20
+
+--14.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007.
+SELECT a.MASP , a.TENSP
+FROM SANPHAM a left join cthd b on a.masp = b.masp
+right join hoadon c on b.sohd = c.sohd 
+WHERE NUOCSX = 'Trung Quoc' or c.nghd = '2007/1/1'
+
+--15.	In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+select a.masp, a.tensp
+from sanpham a
+where masp not in (select masp from cthd)
+
+--16.	In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006.
+with unsold as(
+	select a.sohd, b.masp
+	from hoadon a left join cthd b on a.sohd = b.sohd
+	where year(a.nghd) != 2006
+	)
+select a.masp, a.tensp
+from sanpham a inner join unsold b on a.masp = b.masp
+
+--17.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán được trong năm 2006.
+with unsold as(
+	select a.sohd, b.masp
+	from hoadon a left join cthd b on a.sohd = b.sohd
+	where year(a.nghd) != 2006
+	)
+select a.masp, a.tensp
+from sanpham a inner join unsold b on a.masp = b.masp
+where a.nuocsx ='Trung Quoc'
+--18.	Tìm số hóa đơn đã mua tất cả các sản phẩm do Singapore sản xuất.
+with Bill as(
+	select a.sohd, b.masp
+	from hoadon a inner join cthd b on a.sohd = b.sohd
+	)
+select a.sohd
+from Bill a left join sanpham b on a.masp = b.masp
+where b.nuocsx = 'Singapore'
+--19.	Tìm số hóa đơn trong năm 2006 đã mua ít nhất tất cả các sản phẩm do Singapore sản xuất.
+with bill as(
+	select a.sohd, b.masp
+	from hoadon a left join cthd b on a.sohd = b.sohd
+	where year(a.nghd) = 2006
+	)
+select distinct a.sohd
+from bill a inner join sanpham b on a.masp = b.masp
+where b.nuocsx ='Singapore'
+
+--20.	Có bao nhiêu hóa đơn không phải của khách hàng đăng ký thành viên mua?
+select count(sohd)
+from HOADON
+where MAKH is null
+
+--21.	Có bao nhiêu sản phẩm khác nhau được bán ra trong năm 2006.
+select  count( distinct b.masp)
+from HOADON a left join CTHD b on a.SOHD = b.SOHD
+where year(a.nghd) = 2006
+
+--22.	Cho biết trị giá hóa đơn cao nhất, thấp nhất là bao nhiêu ?
+select max(trigia),min(trigia)
+from HOADON
+
+--23.	Trị giá trung bình của tất cả các hóa đơn được bán ra trong năm 2006 là bao nhiêu?
+select avg(trigia)
+from HOADON
+where year(nghd) = 2006
+
+--24.	Tính doanh thu bán hàng trong năm 2006.
+select sum(trigia) as revenue
+from HOADON
+where year(nghd) = 2006
+
+--25.	Tìm số hóa đơn có trị giá cao nhất trong năm 2006.
+select sohd
+from HOADON
+where trigia in (select max(trigia) from HOADON where year(nghd) = 2006)
+--26.	Tìm họ tên khách hàng đã mua hóa đơn có trị giá cao nhất trong năm 2006.
+with trigia as(
+	select sohd,MAKH
+	from HOADON
+	where trigia in (select max(trigia) from HOADON where year(nghd) = 2006))
+
+select a.HOTEN
+from KHACHHANG a inner join trigia b on a.MAKH = b.MAKH
+
+--27.	In ra danh sách 3 khách hàng đầu tiên (MAKH, HOTEN) sắp xếp theo doanh số giảm dần.
+select top 3 makh,hoten,DOANHSO
+from KHACHHANG
+order by DOANHSO desc
+
+--28.	In ra danh sách các sản phẩm (MASP, TENSP) có giá bán bằng 1 trong 3 mức giá cao nhất.
+select masp,tensp
+from SANPHAM
+where GIA in(	SELECT DISTINCT TOP 3 sp.GIA
+				FROM SANPHAM sp
+				ORDER BY sp.GIA DESC)
+--29.	In ra danh sách các sản phẩm (MASP, TENSP) do “Thai Lan” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của tất cả các sản phẩm).
+select masp,TENSP
+from SANPHAM
+where NUOCSX = 'Thai Lan' and gia in (	SELECT DISTINCT TOP 3 sp.GIA
+										FROM SANPHAM sp
+										ORDER BY sp.GIA DESC)
+
+--30.	In ra danh sách các sản phẩm (MASP, TENSP) do “Trung Quoc” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của sản phẩm do “Trung Quoc” sản xuất).
+select masp,TENSP
+from SANPHAM
+where NUOCSX = 'Trung Quoc' and gia in (SELECT DISTINCT TOP 3 sp.GIA
+										FROM SANPHAM sp
+										where NUOCSX = 'Trung Quoc'
+										ORDER BY sp.GIA DESC)
+--31.* In ra danh sách khách hàng nằm trong 3 hạng cao nhất (xếp hạng theo doanh số).
+select top 3 rank () over (order by doanhso desc) as 'rank', makh,hoten,DOANHSO
+from khachhang
+order by DOANHSO desc
+
+--32.	Tính tổng số sản phẩm do “Trung Quoc” sản xuất.
+select count(masp)
+from SANPHAM
+where NUOCSX = 'Trung Quoc'
+
+--33.	Tính tổng số sản phẩm của từng nước sản xuất.
+select  NUOCSX,count(masp)
+from SANPHAM
+group by NUOCSX
+--34.	Với từng nước sản xuất, tìm giá bán cao nhất, thấp nhất, trung bình của các sản phẩm.
+select  NUOCSX,max(gia) 'Cao Nhat',min(gia)'Thap Nhat',avg(gia)'Trung Binh'
+from SANPHAM
+group by NUOCSX
+
+--35.	Tính doanh thu bán hàng mỗi ngày.
+select nghd,sum(TRIGIA) revenue
+from HOADON
+group by NGHD
+
+--36.	Tính tổng số lượng của từng sản phẩm bán ra trong tháng 10/2006.
+select b.masp,sum(b.sl)
+from HOADON a left join CTHD b on a.SOHD = b.SOHD
+where MONTH(nghd) = 10 and year(nghd) = 2006
+group by b.MASP
+
+--37.	Tính doanh thu bán hàng của từng tháng trong năm 2006.
+select month(nghd),sum(trigia) revenue
+from HOADON
+where year(nghd) = 2006
+group by month(nghd)
+
+--38.	Tìm hóa đơn có mua ít nhất 4 sản phẩm khác nhau.
+select b.SOHD,count(b.masp) as sl
+from HOADON a left join CTHD b on a.SOHD = b.SOHD
+group by b.SOHD
+having count(b.masp) >=4
+
+--39.	Tìm hóa đơn có mua 3 sản phẩm do “Viet Nam” sản xuất (3 sản phẩm khác nhau).
+select b.SOHD , count(b.masp) as num
+from SANPHAM a left join CTHD b on a.MASP = b.MASP
+where a.NUOCSX = 'Viet Nam'
+group by b.SOHD
+having count(b.masp) >=3
+--40.	Tìm khách hàng (MAKH, HOTEN) có số lần mua hàng nhiều nhất. 
+with maxbuy as (
+	select top 1 MAKH, count(sohd) as num
+	from HOADON
+	where MAKH is not null
+	group by MAKH
+)
+select a.makh,a.hoten
+from KHACHHANG a inner join maxbuy b on a.MAKH = b.MAKH
+
+--41.	Tháng mấy trong năm 2006, doanh số bán hàng cao nhất ?
+select top 1 MONTH(nghd) as 'Month',sum(trigia) as 'doanh so'
+from HOADON
+where year(nghd) = 2006
+group by month(nghd)
+order by [doanh so] desc
+
+
+--42.	Tìm sản phẩm (MASP, TENSP) có tổng số lượng bán ra thấp nhất trong năm 2006.
+with total as (
+	select b.MASP,sum(b.sl) as 'total'
+	from HOADON a left join CTHD b on a.SOHD = b.SOHD
+	where year(a.nghd) = 2006
+	group by b.masp
+)
+select top 1 a.MASP,a.TENSP,total
+from SANPHAM a inner join total b on a.MASP = b.MASP
+order by total
+
+--43.	*Mỗi nước sản xuất, tìm sản phẩm (MASP,TENSP) có giá bán cao nhất..
+select MASP,TENSP
+from sanpham a, 
+	(select NUOCSX,max(gia) as 'max gia'
+	from SANPHAM 
+	group by NUOCSX) AS b
+where a.NUOCSX = b.NUOCSX and a.GIA = b.[max gia]
+
+--44.	Tìm nước sản xuất sản xuất ít nhất 3 sản phẩm có giá bán khác nhau.
+select NUOCSX
+from SANPHAM
+group by nuocsx
+having count(distinct gia)>=3
+--45.	*Trong 10 khách hàng có doanh số cao nhất, tìm khách hàng có số lần mua hàng nhiều nhất.
+with buyer as (
+select MAKH,count(sohd) as num
+from HOADON
+group by MAKH)
+
+select top 1 a.MAKH,a.HOTEN,b.num
+from KHACHHANG a left join buyer b on a.MAKH = b.MAKH
+order by num desc
+
+
+
